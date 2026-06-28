e5873dd5bc9064ef5d56f3d234f828b4b06d47a8
Your Name <you@example.com>
2026-06-28 18:42:58 +0300

    Add offers.html design

--- clothes/views.py
+++ clothes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Product
 
-def product_list(request):
+def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
 def offers(request):
     products = Product.objects.all()
     return render(request, 'offers.html', {'products': products})
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from clothes.views import product_list
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', product_list), # هذا يجعل الصفحة الرئيسية تعرض منتجاتك
+    path('', views.home, name='home'),
     path('offers/', views.offers, name='offers'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
